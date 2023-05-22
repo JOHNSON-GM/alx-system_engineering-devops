68ea284b4babdd5264bd0f4981edb866ae588426
JOHNSON-GM <gichukijohnson93@gmail.com>
2023-05-23 01:54:18 +0300

    edit

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
-import csv
-import requests
-import sys
+"""Export data in the CSV format"""
+from csv import writer, QUOTE_ALL
+from csv import QUOTE_ALL
+from sys import argv
+from requests import get
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-        ) for t in todos]
+if __name__ == '__main__':
+    users = get(
+        'https://jsonplaceholder.typicode.com/users/{:s}'
+        .format(str(argv[1]))).json()
+    todos = get(
+        'http://jsonplaceholder.typicode.com/todos?userId={:s}'
+        .format(str(argv[1]))).json()
+    with open('{:s}.csv'.format(argv[1]), mode='w') as f:
+        csv = writer(f, delimiter=',', quotechar='"',
+                     quoting=QUOTE_ALL)
+        for task in todos:
+            csv.writerow([users['id'], users['username'],
+                          task['completed'], task['title']])
